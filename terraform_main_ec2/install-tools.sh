fc5dc4b10951829613b56845a713535189feefb8
Adapaka2023 <132212981+Adapaka2023@users.noreply.github.com>
2024-06-05 18:05:09 -0700

    Update install-tools.sh

--- terraform_main_ec2/install-tools.sh
+++ terraform_main_ec2/install-tools.sh
@@ -24,15 +24,6 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 
-#---------------------------------install tomcat------------------
-#sudo wget url https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.83/bin/apache-tomcat-9.0.83.tar.gz
-#sudo tar -xvzf apache-tomcat-9.0.83.tar.gz #untar
-#cd apache-tomcat-9.0.83
-#cd bin
-#chmod +x startup.sh
-
-
-
 #---------------------------Maven install -------------
 sudo yum install maven -y
 
@@ -68,9 +59,6 @@ sudo service docker start
 #----------------------Trivy install---------------
 sudo rpm -ivh https://github.com/aquasecurity/trivy/releases/download/v0.48.3/trivy_0.48.3_Linux-64bit.rpm
 
-#------------------Docker install-------------
-#sudo amazon-linux-extras install docker #linux 2022
-
 #------------------sonar install by using docker---------------
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
 
@@ -87,22 +75,6 @@ kubectl create namespace Prometheus
 helm install stable prometheus-community/kube-prometheus-stack -n prometheus
 
 echo "Initialization script completed successfully."
-
-#----------------------sonarQube install-----------------------------------
-#sudo yum -y install wget nfs-utils
-#sudo wget -O /etc/yum.repos.d/sonar.repo http://downloads.sourceforge.net/project/sonar-pkg/rpm/sonar.repo
-#sudo yum -y install sonar
-#-----------------------JFROg-----------------------------
-#sudo wget https://releases.jfrog.io/artifactory/artifactory-rpms/artifactory-rpms.repo -O jfrog-artifactory-rpms.repo;
-
-#sudo mv jfrog-artifactory-rpms.repo /etc/yum.repos.d/;
-
-#sudo yum update && sudo yum install jfrog-artifactory-oss -y
-
-#sudo systemctl start artifactory.service
-
-#------------------ Tomcat-----------------------------
-#docker run -d --name tomcat -p 8089:8080 tomcat:lts-community
 
 
 
