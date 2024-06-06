8af8d30323d259649b245ef5e4d7d48a150fcdfa
Adapaka2023 <132212981+Adapaka2023@users.noreply.github.com>
2024-06-05 18:12:09 -0700

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "vsv-vsv-vsv"
-    key    = "eks/terraform.tfstate"
-    region = "us-east-1"
+    bucket = "cloudcore007p1"
+    key    = "eks/maven/terraform.tfstate"
+    region = "ap-south-1"
   }
 
   required_version = ">= 1.6.3"
